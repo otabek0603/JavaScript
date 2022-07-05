144484ec938ca89084a51218caff264c05abe144
otaw5408187 <otaw06032000@gmail.com>
2022-07-05 17:23:02 +0500

    Intial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var ism = prompt("ISMINGIZ:")
+var familya = prompt("FAMILYANGIZ:")
+var e_mail = prompt("E-MAIL:")
+var kasb = prompt("KASBINGIZ:")
+var telefon = prompt("TELEFON NOMERINGIZ:")
+var manzil = prompt("UY MANZILINGIZ")
+
+alert(`ISMINGIZ: ${ism} \n FAMILYANGIZ: ${familya} \n E-MAIL: ${e_mail} \n KASBINGIZ: ${kasb} \n TELEFON NOMERINGIZ: ${telefon} \n UY MANZILINGIZ: ${manzil}`)
+
+console.log(`ISMINGIZ: ${ism} \n FAMILYANGIZ: ${familya} \n E-MAIL: ${e_mail} \n KASBINGIZ: ${kasb} \n TELEFON NOMERINGIZ: ${telefon} \n UY MANZILINGIZ: ${manzil}`);
+
+// Mansurov Otabek
